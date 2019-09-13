b0de1acd68b279ee77e8a5d578bbf5ac80c18f1a
Sam Belliveau <sam.belliveau@gmail.com>
2019-09-13 17:23:32 -0400

    Add Drivetrain Stuff. (continue in same format)

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,13 +14,89 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+    interface Drivetrain {
+        interface Ports {
+            int LEFT_TOP_MOTOR = 2;
+            int LEFT_MIDDLE_MOTOR = 3;
+            int LEFT_BOTTOM_MOTOR = 1;
+        
+            int RIGHT_TOP_MOTOR = 6;
+            int RIGHT_MIDDLE_MOTOR = 7;
+            int RIGHT_BOTTOM_MOTOR = 5;
+        
+            int GEAR_SHIFT = 0;
+        }
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+        interface Encoder {
+            int LEFT_CHANNEL_A = 2;
+            int LEFT_CHANNEL_B = 3;
+        
+            int RIGHT_CHANNEL_A = 0;
+            int RIGHT_CHANNEL_B = 1;
+        }
+
+        interface Motor {
+            double WHEEL_DIAMETER = 6;
+            double WHEEL_INCHES_PER_REVOLUTION = WHEEL_DIAMETER * Math.PI;
+
+            double NEO_ENCODER_EMPIRICAL_MULTIPLIER = 1;
+            double NEO_ENCODER_RAW_MULTIPLIER = WHEEL_INCHES_PER_REVOLUTION * NEO_ENCODER_EMPIRICAL_MULTIPLIER;
+
+            double RAMP_RATE = 0.3;
+        }
+
+        interface Greyhill {
+            double PULSES_PER_REVOLUTION = 256 * 4.0;
+            double OUTER_GEAR_RATIO = 24.0 / 60.0;
+            double ENCODER_EMPIRICAL_MULTIPLIER = 1.3;
+            double INCHES_PER_PULSE = ((Motor.WHEEL_INCHES_PER_REVOLUTION * Motor.DRIVETRAIN_OUTER_GEAR_RATIO) / PULSES_PER_REVOLUTION) * ENCODER_EMPIRICAL_MULTIPLIER;
+
+            int CURRENT_LIMIT = 65;
+        }
+
+        interface Command {        // Prints Information to Smartdashboard
+            boolean SMARTDASHBOARD_DEBUG = true;
+    
+            interface Pipeline {
+                int DRIVER = 0;
+                int CV = 1;
+            }
+        
+            interface CV { 
+            }
+    
+            interface TurnSpeed {
+                // Max speed robot should turn
+                // Every value after this is effectivly 
+                // multiplied by this value
+                double MAX = 2.0 / 3.0;
+    
+                // Speed at which robot moves during nudging
+                double NUDGE = 1.0/3.0;
+            }
+    
+            interface QuickTurn {
+                // Speed at which curvature drive enables quick turn
+                double THRESHOLD = 0.04;
+    
+                // Speed at which drivetrain turns when using quick turn
+                double SPEED = 2.0/3.0;
+            }
+    
+            // What percent that quickturn speeds up (Higher = Slower)
+            interface SmoothTurn {
+                // Enables or Disables "Smooth Turn"
+                boolean ENABLED = false;
+    
+                double ACCEL = 36;
+                double DECEL = 2;
+            }
+    
+            interface Controls {
+                // Scaling joysticks and triggers using exponents
+                int JOYSTICK_SCALAR = 3;
+                int TRIGGER_SCALAR = 2;
+            }
+        }
+    }
 }
---
+++ src/main/java/frc/robot/commands/DrivetrainCommand.java
@@ -0,0 +1,95 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.RobotMap.Drivetrain;
+import frc.robot.Robot;
+
+public abstract class DrivetrainCommand extends Command {
+
+    // Helper class used to get gamepad controls
+    public static class Controller {
+        public static double getTriggers() {
+            double mSpeed;
+            
+            mSpeed = Math.pow(Robot.oi.driverGamepad.getRawRightTriggerAxis(), Drivetrain.Command.Controls.TRIGGER_SCALAR);
+            mSpeed -= Math.pow(Robot.oi.driverGamepad.getRawLeftTriggerAxis(), Drivetrain.Command.Controls.TRIGGER_SCALAR);
+            
+            return mSpeed;
+        }
+
+        @SuppressWarnings("unused")
+        public static double getJoystick() {
+            double leftStick = Robot.oi.driverGamepad.getLeftX();
+            leftStick = Math.pow(leftStick, Drivetrain.Command.Controls.JOYSTICK_SCALAR);
+    
+            if (Drivetrain.Controls.JOYSTICK_SCALAR % 2 == 0) {
+                leftStick *= Math.signum(Robot.oi.driverGamepad.getLeftX());
+            }
+
+            return leftStick;
+        }
+    }
+    
+    // Variables to feed to curvature drive
+    protected double mSpeed = 0;
+    protected double mTurn = 0; 
+    protected boolean mQuickTurn = true;
+
+    public DrivetrainCommand() {
+        requires(Robot.drivetrain);
+    }
+
+    @Override
+    protected void execute() {
+        setCameraMode();
+        setSpeed();
+        setTurn();
+        setQuickTurn();
+        updateSmartdashboard();
+        updateDrivetrain();
+    }
+
+    /* Current mode of the drivetrain */
+    public enum States { START, DRIVER, CV, OTHER; }
+    protected static States mState = States.START;
+
+    /* Updating limelight camera */
+    protected abstract void setCameraMode();
+
+    /* Updating Speed */
+    protected abstract void setSpeed();
+
+    /* Updating Turning */
+    protected abstract void setTurn();
+
+    /* Updating Quick Turn */
+    protected abstract void setQuickTurn();
+
+    /* Updating SmartDashboard */
+    protected void updateSmartdashboard() {
+        if(Drivetrain.SMARTDASHBOARD_DEBUG) {
+            SmartDashboard.putNumber("Drivetrain Speed", mSpeed);
+            SmartDashboard.putNumber("Drivetrain Turn", mTurn);
+            SmartDashboard.putBoolean("Drivetrain QuickTurn", mQuickTurn);
+            SmartDashboard.putString("Drivetrain Mode", mState.name());
+        }
+    }
+
+    // Sub commands for each curvature drive variable
+    protected void updateDrivetrain() {
+        Robot.drivetrain.curvatureDrive(mSpeed, mTurn, mQuickTurn);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
